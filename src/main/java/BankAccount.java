739258e190fc99445361840b91fe3a8ce6528075
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-05-09 17:02:14 +0100

    added tests and methods for getters,setters,deposit and withdrawal

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -2,11 +2,11 @@ import java.time.LocalDate;
 
 public class BankAccount {
 
-    String firstName;
-    String lastName;
-    LocalDate dateOfBirth;
-    String accountNumber;
-    double balance;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String accountNumber;
+    private double balance;
 
     public BankAccount(String firstName, String lastName, LocalDate dateOfBirth, String accountNumber) {
         this.firstName = firstName;
@@ -15,6 +15,55 @@ public class BankAccount {
         this.accountNumber = accountNumber;
         this.balance = 0;
     }
+//    Getters
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public Double getBalance() {
+        return this.balance;
+    }
+//    Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+//    Other methods
+    public void deposit(Double amount) {
+        this.balance += amount;
+    }
+
+    public void withdrawal(Double amount) {
+        this.balance = this.balance - amount;
+    }
+
+
+
+
 
 
 
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,2 +1,105 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BankAccountTest {
+
+    BankAccount bankAccount;
+
+    @BeforeEach
+    public void setUp() {
+        bankAccount = new BankAccount("John", "Smith", LocalDate.of(1980,1,1), "12345678");
+    }
+//    Testing getters
+    @Test
+    public void canGetFirstName() {
+        String result = bankAccount.getFirstName();
+        assertThat(result).isEqualTo("John");
+    }
+
+    @Test
+    public void canGetLastName() {
+        String result = bankAccount.getLastName();
+        assertThat(result).isEqualTo("Smith");
+    }
+
+    @Test
+    public void canGetDateOfBirth() {
+        LocalDate result = bankAccount.getDateOfBirth();
+        assertThat(result).isEqualTo(LocalDate.of(1980, 1, 1));
+    }
+
+    @Test
+    public void canGetAccountNumber() {
+        String result = bankAccount.getAccountNumber();
+        assertThat(result).isEqualTo("12345678");
+    }
+
+    @Test
+    public void canGetBalance() {
+        Double result = bankAccount.getBalance();
+        assertThat(result).isEqualTo(0);
+    }
+//    Testing setters
+    @Test
+    public void canSetFirstName() {
+        bankAccount.setFirstName("Jane");
+        String result = bankAccount.getFirstName();
+        assertThat(result).isEqualTo("Jane");
+    }
+
+    @Test
+    public void canSetLastName() {
+        bankAccount.setLastName("Doe");
+        String result = bankAccount.getLastName();
+        assertThat(result).isEqualTo("Doe");
+    }
+
+    @Test
+    public void canSetDateOfBirth() {
+        bankAccount.setDateOfBirth(LocalDate.of(1970,2,2));
+        LocalDate result = bankAccount.getDateOfBirth();
+        assertThat(result).isEqualTo(LocalDate.of(1970, 2, 2));
+    }
+
+    @Test
+    public void canSetAccountNumber() {
+        bankAccount.setAccountNumber("87654321");
+        String result = bankAccount.getAccountNumber();
+        assertThat(result).isEqualTo("87654321");
+    }
+
+    @Test
+    public void canDepositMoney() {
+        bankAccount.deposit(100.0);
+        Double result = bankAccount.getBalance();
+        assertThat(result).isEqualTo(100);
+    }
+
+    @Test
+    public void canWithdrawMoney() {
+        bankAccount.deposit(100.0);
+        bankAccount.withdrawal(50.0);
+        Double result = bankAccount.getBalance();
+        assertThat(result).isEqualTo(50.0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
